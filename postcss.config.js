b9f1ceea99391bc3c2f847285bf708aba7591ba0
Joakim Larsson <joakim.larsson@ivytech.se>
2025-12-16 13:44:51 +0100

    refactor: update PostCSS configuration to use object syntax for plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,12 @@
 module.exports = {
-  plugins: [
-    require("postcss-preset-env")({
+  plugins: {
+    "postcss-preset-env": {
       stage: 4,
       features: {
         "nesting-rules": true,
         "custom-media-queries": true,
       },
-    }),
-    require("autoprefixer"),
-  ],
+    },
+    autoprefixer: {},
+  },
 };
-
